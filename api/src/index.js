d3d320684b89f41d32595b100966a0102f393dc9
Edwin Ade <edwinmbonyjr@gmail.com>
2024-12-24 22:54:06 +0100

    update: hostname

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,7 @@
 import app from './app.js';
 import { PORT } from './utils/config.js';
 
-app.listen(PORT, (err) => {
+app.listen(PORT, '127.0.0.1', (err) => {
   if (err) console.error('Error in server setup:', err.message);
   console.log('Server listening on port', PORT);
 });
