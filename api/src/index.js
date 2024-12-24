57c5bc4e9c92c5820bf28156c56cd5b3401a1946
Edwin Ade <edwinmbonyjr@gmail.com>
2024-12-24 23:02:32 +0100

    fix: bind to 0.0.0.0

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,7 @@
 import app from './app.js';
 import { PORT } from './utils/config.js';
 
-app.listen(PORT, '127.0.0.1', (err) => {
+app.listen(PORT, '0.0.0.0', (err) => {
   if (err) console.error('Error in server setup:', err.message);
   console.log('Server listening on port', PORT);
 });
