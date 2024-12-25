2b84b8310fce903a89e586cdc4933b2848a6af13
Edwin Ade <edwinmbonyjr@gmail.com>
2024-12-25 09:14:19 +0100

    feat: favicon support for browser users

--- api/src/app.js
+++ api/src/app.js
@@ -1,11 +1,14 @@
 import express from 'express';
+import * as path from 'path';
 import cors from 'cors';
 import * as middleware from './utils/middleware.js';
 import playersRouter from './controllers/players.js';
 import clubsRouter from './controllers/clubs.js';
 
 const app = express();
-
+app.get('/favicon.ico', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'assets', 'favicon.ico'));
+});
 app.use(cors());
 app.use(express.json());
 app.use(middleware.logger);
