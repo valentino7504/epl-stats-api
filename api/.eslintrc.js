49b662bdc5f2a16cd22e449ae0b660030bfb446d
Edwin Ade <edwinmbonyjr@gmail.com>
2024-12-25 09:23:04 +0100

    chore: update eslintrc

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -8,6 +8,14 @@ module.exports = {
     'no-underscore-dangle': 0,
     'no-param-reassign': 0,
     'no-return-assign': 0,
+    'no-console': 0,
+    'max-len': ['error', { code: 80 }],
     camelcase: 0,
+    'import/extensions': [
+      'error',
+      {
+        js: 'ignorePackages',
+      },
+    ],
   },
 };
