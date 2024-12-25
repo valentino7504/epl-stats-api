43739f9fa021438828897dffdc42180f97e20545
Edwin Ade <edwinmbonyjr@gmail.com>
2024-12-25 09:25:24 +0100

    fix: strip unwanetd created at and updated at columns

--- api/src/utils/stripUnwantedColumns.js
+++ api/src/utils/stripUnwantedColumns.js
@@ -1,6 +1,8 @@
 export default function stripTimeStamps(result) {
   return result.map((item) => {
-    const { created_at, updated_at, ...filteredItem } = item;
+    const {
+      created_at, updated_at, createdAt, updatedAt, ...filteredItem
+    } = item;
     return filteredItem;
   });
 }
