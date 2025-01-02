a883a5788e4e33977218d96b30754422ea695eb7
Edwin Ade <edwinmbonyjr@gmail.com>
2025-01-02 19:18:56 +0100

    feat: add collection routes

---
+++ api/src/routes/collections.js
@@ -0,0 +1,121 @@
+import { Router } from 'express';
+import { authenticateToken } from '../utils/middleware.js';
+import {
+  createCollection, deleteCollection, getCollectionById,
+  updateCollectionMetadata, getCollectionsByName,
+  getAllUserCollections,
+  addPlayersToCollection,
+  addClubsToCollection,
+  removePlayersFromCollection,
+  removeClubsFromCollection,
+} from '../services/collectionsService.js';
+
+const collectionsRouter = Router();
+
+collectionsRouter.use(authenticateToken);
+
+collectionsRouter.get('/', async (req, res) => {
+  const userId = req.user.id;
+  const { name } = req.query;
+  let collections;
+  if (name) {
+    collections = await getCollectionsByName(name, userId);
+  } else {
+    collections = await getAllUserCollections(userId);
+  }
+  return res.status(200).json(collections);
+});
+
+collectionsRouter.post('/', async (req, res) => {
+  const userId = req.user.id;
+  const { name, description } = req.body;
+  try {
+    const collection = await createCollection(userId, { name, description });
+    return res.status(200).json(collection);
+  } catch (err) {
+    return res.status(400).json({ error: 'Bad request', message: err.message });
+  }
+});
+
+collectionsRouter.get('/:id', async (req, res) => {
+  const userId = req.user.id;
+  const collectionId = req.params.id;
+  try {
+    const collection = await getCollectionById(userId, collectionId);
+    return res.status(200).json(collection);
+  } catch (err) {
+    return res.status(404).json({ error: 'Not Found', message: err.message });
+  }
+});
+
+collectionsRouter.delete('/:id', async (req, res) => {
+  const userId = req.user.id;
+  const collectionId = req.params.id;
+  try {
+    await deleteCollection(collectionId, userId);
+    return res.status(204).end();
+  } catch (err) {
+    return res.status(404).json({ error: 'Not Found', message: err.message });
+  }
+});
+
+collectionsRouter.post('/:id', async (req, res) => {
+  const userId = req.user.id;
+  const collectionId = req.params.id;
+  try {
+    const updatedCollection = await updateCollectionMetadata(collectionId, userId, req.body);
+    return res.status(200).json(updatedCollection);
+  } catch (err) {
+    return res.status(400).json({ err: 'Bad request', message: err.message });
+  }
+});
+
+collectionsRouter.post('/:id/players', async (req, res) => {
+  const userId = req.user.id;
+  const collectionId = req.params.id;
+  const { players } = req.body;
+  try {
+    const collection = await addPlayersToCollection(collectionId, userId, players);
+    return res.status(200).json(collection);
+  } catch (err) {
+    return res.status(400).json({ error: 'Bad request', message: err.message });
+  }
+});
+
+collectionsRouter.post('/:id/clubs', async (req, res) => {
+  const userId = req.user.id;
+  const collectionId = req.params.id;
+  const { clubs } = req.body;
+  try {
+    const collection = await addClubsToCollection(collectionId, userId, clubs);
+    return res.status(200).json(collection);
+  } catch (err) {
+    return res.status(400).json({ error: 'Bad request', message: err.message });
+  }
+});
+
+collectionsRouter.delete('/:id/players', async (req, res) => {
+  const userId = req.user.id;
+  const collectionId = req.params.id;
+  const { players } = req.body;
+  try {
+    await removePlayersFromCollection(collectionId, userId, players);
+    return res.status(204).end();
+  } catch (err) {
+    return res.status(400).json({ error: 'Bad request', message: err.message });
+  }
+});
+
+collectionsRouter.delete('/:id/clubs', async (req, res) => {
+  const userId = req.user.id;
+  const collectionId = req.params.id;
+  const { clubs } = req.body;
+  try {
+    await removeClubsFromCollection(collectionId, userId, clubs);
+    return res.status(204).end();
+  } catch (err) {
+    return res.status(400).json({ error: 'Bad request', message: err.message });
+  }
+});
+
+export default collectionsRouter;
---
+++ api/src/services/collectionsService.js
@@ -0,0 +1,190 @@
+import {
+  and, eq, inArray, sql,
+} from 'drizzle-orm';
+import collections from '../models/collections.js';
+import db from '../utils/dbManager.js';
+import players from '../models/players.js';
+import { playersToCollections } from '../models/collectionsPlayers.js';
+import clubs from '../models/clubs.js';
+import { clubsToCollections } from '../models/collectionsClubs.js';
+
+async function getPlayersAndClubs(collection) {
+  const collectionPlayers = await db
+    .select(players)
+    .from(playersToCollections)
+    .innerJoin(players, eq(playersToCollections.playerId, players.id))
+    .where(eq(playersToCollections.collectionId, collection.id));
+  const collectionClubs = await db
+    .select(clubs)
+    .from(clubsToCollections)
+    .innerJoin(clubs, eq(clubsToCollections.clubId, clubs.id))
+    .where(eq(clubsToCollections.collectionId, collection.id));
+  return { ...collection, players: collectionPlayers, clubs: collectionClubs };
+}
+
+export async function createCollection(userId, info) {
+  const { name, description } = info;
+  try {
+    const collection = await db
+      .insert(collections)
+      .values({ userId, name, description })
+      .returning({
+        id: collections.id,
+        userId: collections.userId,
+        name: collections.name,
+        description: collections.description,
+      });
+    return collection[0];
+  } catch (err) {
+    if (parseInt(err.code, 10) === 23505 || err.message.toLowerCase().includes('unique')) {
+      throw new Error('Collection with this name already exists for this user');
+    }
+    throw new Error(err.message);
+  }
+}
+
+export async function updateCollectionMetadata(collectionId, userId, updatedDetails) {
+  const { name, description } = updatedDetails;
+  const updatedObj = { updatedAt: new Date() };
+  if (name) {
+    updatedObj.name = name;
+  }
+  if (description) {
+    updatedObj.description = description;
+  }
+  const collectionArr = await db
+    .update(collections)
+    .set(updatedObj)
+    .where(and(eq(collections.id, collectionId), eq(collections.userId, userId)))
+    .returning({
+      id: collections.id,
+      userId: collections.userId,
+      name: collections.name,
+      description: collections.description,
+    });
+  if (collectionArr.length < 1) {
+    throw new Error('No collection with this id belonging to this user');
+  }
+  return getPlayersAndClubs(collectionArr[0]);
+}
+
+export async function deleteCollection(collectionId, userId) {
+  const collectionArr = await db
+    .delete(collections)
+    .where(and(eq(collections.id, collectionId), eq(collections.userId, userId)))
+    .returning({ id: collections.id });
+  if (collectionArr.length < 1) {
+    throw new Error('Collection does not exist for the authenticated user');
+  }
+}
+
+export async function getCollectionById(userId, collectionId) {
+  const collectionArr = await db
+    .select()
+    .from(collections)
+    .where(and(eq(collections.id, collectionId), eq(collections.userId, userId)));
+  if (collectionArr.length < 1) {
+    throw new Error('Collection does not exist for the authenticated user');
+  }
+  const collection = await getPlayersAndClubs(collectionArr[0]);
+  return collection;
+}
+
+export async function getAllUserCollections(userId) {
+  const collectionArr = await db.select().from(collections).where(eq(collections.userId, userId));
+  const collectionsData = Promise.all(collectionArr.map(getPlayersAndClubs));
+  return collectionsData;
+}
+
+/* eslint-disable prefer-template */
+export async function getCollectionsByName(name, userId) {
+  const collectionArr = await db.execute(
+    sql`SELECT * FROM ${collections} WHERE unaccent(${collections.name})
+    ILIKE unaccent(${'%' + name + '%'}) AND ${collections.userId} = ${'' + userId}`,
+  );
+  if (collectionArr.rows.length < 1) {
+    return [];
+  }
+  const collectionsData = Promise.all(collectionArr.rows.map(getPlayersAndClubs));
+  return collectionsData;
+}
+/* eslint-enable prefer-template */
+
+export async function addPlayersToCollection(collectionId, userId, playerIds) {
+  const playersArray = playerIds.map((playerId) => ({ collectionId, playerId }));
+  const collection = await db
+    .select()
+    .from(collections)
+    .where(and(eq(collections.id, collectionId), eq(collections.userId, userId)));
+  if (collection.length === 0) {
+    throw new Error('Collection does not belong to the authorized user');
+  }
+  try {
+    await db.insert(playersToCollections).values(playersArray);
+  } catch (err) {
+    if (parseInt(err.code, 10) === 23503) {
+      throw new Error('Invalid player ID');
+    }
+  }
+  return getCollectionById(userId, collectionId);
+}
+
+export async function addClubsToCollection(collectionId, userId, clubIds) {
+  const clubsArray = clubIds.map((clubId) => ({ collectionId, clubId }));
+  await db.transaction(async (tx) => {
+    const collection = await tx
+      .select()
+      .from(collections)
+      .where(and(eq(collections.id, collectionId), eq(collections.userId, userId)));
+    if (collection.length === 0) {
+      throw new Error('Collection does not belong to the authorized user');
+    }
+    try {
+      await tx.insert(clubsToCollections).values(clubsArray);
+    } catch (err) {
+      if (parseInt(err.code, 10) === 23503) {
+        throw new Error('Invalid club ID');
+      }
+    }
+  });
+  return getCollectionById(userId, collectionId);
+}
+
+export async function removePlayersFromCollection(collectionId, userId, playerIds) {
+  await db.transaction(async (tx) => {
+    const collection = await tx
+      .select()
+      .from(collections)
+      .where(and(eq(collections.id, collectionId), eq(collections.userId, userId)));
+    if (collection.length === 0) {
+      throw new Error('Collection does not belong to the authorized user');
+    }
+    try {
+      await tx.delete(playersToCollections)
+        .where(inArray(playersToCollections.playerId, playerIds));
+    } catch (err) {
+      if (parseInt(err.code, 10) === 23503) {
+        throw new Error('Invalid player ID in array');
+      }
+    }
+  });
+}
+
+export async function removeClubsFromCollection(collectionId, userId, clubIds) {
+  await db.transaction(async (tx) => {
+    const collection = await tx
+      .select()
+      .from(collections)
+      .where(and(eq(collections.id, collectionId), eq(collections.userId, userId)));
+    if (collection.length === 0) {
+      throw new Error('Collection does not belong to the authorized user');
+    }
+    try {
+      await tx.delete(clubsToCollections).where(inArray(clubsToCollections.clubId, clubIds));
+    } catch (err) {
+      if (parseInt(err.code, 10) === 23503) {
+        throw new Error('Invalid club ID');
+      }
+    }
+  });
+}
